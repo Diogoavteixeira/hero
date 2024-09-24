feccda2ff7832cfb399c0f3f3de559536302007b
Diogo Teixeira <A044483@umaia.pt>
2024-09-24 22:10:34 +0100

    commit4

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,15 +1,20 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
-
+;
 public class Game {
     private final TerminalScreen screen;
+    private int x = 10;
+    private int y = 10;
     public Game(int  width, int height) throws IOException {
+
 
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
         screen = new TerminalScreen(terminal);
@@ -21,14 +26,38 @@ public class Game {
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(0, 0, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(0, 30, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(30, 0, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(30, 30, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
+
     }
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+    private void processKey(KeyStroke key) {
+       System.out.println(key);
+        String keyT = key.getKeyType().toString();
+        switch (keyT) {
+            case "ArrowUp":
+                y = y - 1;
+                break;
+            case "ArrowDown":
+                y = y + 1;
+                break;
+            case "ArrowLeft":
+                x = x - 1;
+                break;
+            case "ArrowRight":
+                x = x + 1;
+                break;
+        }
     }
 }
 
