515618124c66e0c5d92339929adbaa53b87d1f85
Diogo Teixeira <A044483@umaia.pt>
2024-10-01 11:54:57 +0100

    exercicio13

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Position {
     private int x ;
     private int y;
@@ -11,15 +13,28 @@ public class Position {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
+    //public void setX(int x) {
+        //this.x = x;
+    //}
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    //public void setY(int y) {
+        //this.y = y;
+    //}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
 }
